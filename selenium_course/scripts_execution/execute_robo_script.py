6dec60c9f6c88b0cb05213b4ccda9affc89a5aa6
FlauntyNoize <FlauntyNoize@users.noreply.github.com>
2020-05-28 01:30:42 +0300

    Create execute_robo_script.py
    Scroll page using execute_script

---
+++ selenium_course/scripts_execution/execute_robo_script.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/execute_script.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    
+    browser.execute_script("window.scrollBy(0, 100);")
+
+    result = browser.find_element_by_id("answer")
+    result.send_keys(y)
+
+    robo_check = browser.find_element_by_id("robotCheckbox")
+    robo_check.click()
+
+    robo_radio = browser.find_element_by_id("robotsRule")
+    robo_radio.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
