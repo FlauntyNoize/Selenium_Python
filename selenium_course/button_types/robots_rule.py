8ad6591835903bfda61eff0554e405dc6ff2acc6
FlauntyNoize <FlauntyNoize@users.noreply.github.com>
2020-05-28 01:32:40 +0300

    Create robots_rule.py
    Learning to work with checkboxes and radiobuttons

---
+++ selenium_course/button_types/robots_rule.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    
+    result = browser.find_element_by_id("answer")
+    result.send_keys(y)
+
+    robo_check = browser.find_element_by_id("robotCheckbox")
+    robo_check.click()
+
+    robo_radio = browser.find_element_by_id("robotsRule")
+    robo_radio.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
